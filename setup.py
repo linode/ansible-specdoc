6c55a257138e5de929c86801e284a7b66c30d890
Zhiwei Liang <121905282+zliang-akamai@users.noreply.github.com>
2023-06-22 15:07:23 -0400

    Migrate build front-end to `pypa/build` (#18)

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 import os
+from pathlib import Path
 
 import setuptools
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-def get_long_description():
-    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = f.read()
-
-    return long_description
+current_dir = Path(__file__).parent.resolve()
+readme_path = current_dir / "README.md"
+requirements_path = current_dir / "requirements.txt"
 
 def get_version():
     version_env = os.getenv("SPECDOC_VERSION")
@@ -25,21 +22,15 @@ setuptools.setup(
     author="Linode",
     author_email="dev-dx@linode.com",
     description="A simple tool for generating Ansible collection documentation from module spec.",
-    long_description=get_long_description(),
+    long_description=readme_path.read_text(),
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
     keywords="ansible",
     url="https://github.com/linode/ansible-specdoc/",
-    packages=['ansible_specdoc'],
-    install_requires=[
-        'PyYAML==5.4.1',
-        'Jinja2==3.0.1',
-        'redbaron==0.9.2'
-    ],
-    setup_requires=['setupext_janitor'],
-    python_requires='>=3',
+    packages=["ansible_specdoc"],
+    install_requires=requirements_path.read_text().splitlines(),
+    python_requires=">=3",
     entry_points={
-        'console_scripts': ['ansible-specdoc=ansible_specdoc.cli:main'],
-        'distutils.commands': ['clean = setupext_janitor.janitor:CleanCommand']
-    }
+        "console_scripts": ["ansible-specdoc=ansible_specdoc.cli:main"],
+    },
 )
